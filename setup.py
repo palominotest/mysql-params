95daf3d4230337ef87751c77e6c39e8fc19d70af
Elmer Medez <emedez@palominodb.com>
2013-09-25 16:06:53 +0000

    updated setup files

--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 setup(
-    name='mysql-params',
+    name='python27-mysql-params',
     version='0.1b.dev',
     author='PalominoDB',
     author_email='oss@palominodb.com',
     packages=find_packages(),
+    package_data={
+        '': ['*.template', 'fixtures/*.json', 'static-raw/css/*.css', 'static-raw/img/*',
+            'static-raw/js/*', 'templates/rds/*.html', 'templates/*.html']
+    }, 
     #data_files=[('docs', ['docs/args.sample.txt', 'docs/config_sample.yml', 'docs/logging.sample.cnf'])],
     url="http://pypi.python.org/pypi/mysql-params",
     license='GPLv2',
@@ -30,17 +34,17 @@ setup(
 #hurry.filesize==0.9
     install_requires=[
         'Django>=1.5.1',
-        'MySQL-python>=1.2.4',
+        'MySQL-python>=1.2',
         #'South==dev',
-        'South>=0.8.1',
+        'South>=0.8.2',
         'argparse>=1.2.1',
         #'boto==dev',
-        'boto>=2.9.9',
+        'boto>=2.9.5',
         #'jsonfield==dev',
         'jsonfield>=0.9.18',
         'texttable==0.8.1',
         'colorama>=0.2.5',
-        'paramiko>=1.10.1',
+        'paramiko>=1.7.5',
         'hurry.filesize>=0.9'
     ],
     #dependency_links=[
@@ -48,9 +52,9 @@ setup(
     #    'git+https://github.com/rbondoc-palominodb/boto.git@68365dafdeb88f4f4aab06bf6f4e8f11064f169f#egg=boto-dev',
     #    'git+https://github.com/bradjasper/django-jsonfield.git@49e00d20c6ee9f10e14286e8d709b25f271d008f#egg=jsonfield-dev'
     #],
-    #entry_points={
-    #    'console_scripts': [
-    #        'pdb_check_maxvalue = int_overflow_check.pdb_check_maxvalue:main',
-    #    ]
-    #}
+    entry_points={
+        'console_scripts': [
+            'python27-mysql-params-manage = mysqlparamsproject.manage',
+        ]
+    }
 )
